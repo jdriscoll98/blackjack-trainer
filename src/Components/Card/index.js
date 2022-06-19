a5ec59e309f3bc3d6c3a70d5d2783fcdc0fa54a4
Jack Driscoll <jdriscoll98@ufl.edu>
2022-06-18 23:30:55 -0400

    cards get dealt out

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getSuitChar } from "../../Utils/";
 import "./Card.css";
 
 export function Card(props) {
   const [number] = useState(props.number ?? "A");
   const [suit] = useState(props.suit ?? "spades");
+  const [isHidden, setIsHidden] = useState(props.showDelay);
+
+  useEffect(() => {
+    if (props.showDelay) {
+      setTimeout(() => {
+        setIsHidden(false);
+      }, props.showDelay);
+    }
+  }, [props.showDelay]);
   return (
     <div
-      className={`card ${props.className} ${props.flipped ? "flipped" : ""}`}
+      className={`card ${props.className} ${props.flipped ? "flipped" : ""} ${
+        isHidden ? "hidden" : ""
+      }  card-color-${["hearts", "diamonds"].includes(suit) ? "red" : "black"}`}
     >
-      <div className="card-number-top">{number}</div>
-      <div className="card-suit">{getSuitChar(suit)}</div>
-      <div className="card-number-bottom">{number}</div>
+      <div className="card-number-top">{!props.flipped ? number : ""}</div>
+      <div className="card-suit">{!props.flipped ? getSuitChar(suit) : ""}</div>
+      <div className="card-number-bottom">{!props.flipped ? number : ""}</div>
     </div>
   );
 }
--- src/Pages/CardCounting/Game/States/Deal.js
+++ src/Pages/CardCounting/Game/States/Deal.js
@@ -24,17 +24,31 @@ export function DealState(props) {
   return (
     <>
       <Card className="deck" flipped></Card>
-      {dealerCards.map((card, index) => (
-        <Card
-          className={`dealer card-${index + 1}`}
-          key={`dealer-${index + 1}`}
-          suit={card.suit}
-          number={card.number}
-        ></Card>
-      ))}
+      {dealerCards.map((card, index) => {
+        const showDelay =
+          index === 0
+            ? (props.tablePlayerCount + 1) * 1000
+            : (props.tablePlayerCount * 2 + 2) * 1000;
+        return (
+          <Card
+            className={`dealer card-${index + 1}`}
+            key={`dealer-${index + 1}`}
+            suit={card.suit}
+            number={card.number}
+            id={`dealer-${index + 1}`}
+            showDelay={showDelay}
+            flipped={index === 0}
+          ></Card>
+        );
+      })}
       {playerCards.map((card, index) => {
         const player = (index % props.tablePlayerCount) + 1;
         const numberOfCard = index < props.tablePlayerCount ? "1" : "2";
+        const showDelay =
+          player * 1000 +
+          (index >= props.tablePlayerCount
+            ? props.tablePlayerCount * 1000 + 1000
+            : 0);
 
         return (
           <Card
@@ -42,6 +56,7 @@ export function DealState(props) {
             suit={card.suit}
             key={`${card.number}-${card.suit}-${index}`}
             className={`player-${player} card-${numberOfCard}`}
+            showDelay={showDelay}
           ></Card>
         );
       })}
